a3d820dae06cf39ae34433092e561f7e332d16f5
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2018-09-26 13:22:26 -0400

    Update to 3.1.1.

--- release/src/mode.js
+++ release/src/mode.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-typescript version: 2.0.1(95b555183e942e71517ec1d8a17ee8f3f05d9e8e)
+ * monaco-typescript version: 2.0.1(264d2be60960abd7d9a25da69a82989ef305b2e0)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-typescript/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
